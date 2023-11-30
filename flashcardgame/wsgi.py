aac6caad8f7261b85e3b75c573bf62740bd66e00
Calvin G. <calcal56@gmail.com>
2023-11-29 19:03:13 -0600

    troubleshjooting dpeloyment

--- flashcardgame/wsgi.py
+++ flashcardgame/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashcardgame.productionsettings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashcardgame.settings')
 
 application = get_wsgi_application()
 
