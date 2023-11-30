817b0824e9c377c2b1bc75215b8dca0d36007061
Calvin G. <calcal56@gmail.com>
2023-11-29 22:22:44 -0600

    >:(

--- flashcardgame/productionsettings.py
+++ flashcardgame/productionsettings.py
@@ -82,9 +82,9 @@ INSTALLED_APPS = [
     
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
