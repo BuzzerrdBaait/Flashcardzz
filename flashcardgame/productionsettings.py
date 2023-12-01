b5c823847ac19c8c909de540249710329f84940b
Calvin G. <calcal56@gmail.com>
2023-12-01 08:49:51 -0600

    >:(

--- flashcardgame/productionsettings.py
+++ flashcardgame/productionsettings.py
@@ -78,9 +78,8 @@ COMPRESS_URL= CLOUDFRONT_URL
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# Use S3 for media files storage
 
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
