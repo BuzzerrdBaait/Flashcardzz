e23011a705d9987e8a2174cd7d2810efb4584776
Calvin G. <calcal56@gmail.com>
2023-12-02 10:42:00 -0600

    removing heroku db

--- flashcardgame/productionsettings.py
+++ flashcardgame/productionsettings.py
@@ -143,13 +143,23 @@ DB_PASSWORD= os.environ.get('DB_PASSWORD')
 ########## H E R O K U    B U L L S H I T #############
 if IS_HEROKU_APP:
 
+
+    
     DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
+
+
+#    DATABASES = {
+#        "default": dj_database_url.config(
+#            conn_max_age=600,
+#            conn_health_checks=True,
+#            ssl_require=True,
+#        ),
+#    }
 
 #######################################################
 else:
