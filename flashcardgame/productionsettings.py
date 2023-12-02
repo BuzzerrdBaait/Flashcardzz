156d31c2fd2f0d9dbc2cfe327da4fd913248f0f2
Calvin G. <calcal56@gmail.com>
2023-12-02 10:52:40 -0600

    the file paths wasn't the issue

--- flashcardgame/productionsettings.py
+++ flashcardgame/productionsettings.py
@@ -115,7 +115,7 @@ ROOT_URLCONF = 'flashcardgameapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'flashcardgameapp', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'flashcardgameapp', 'templates','flashcardgameapp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
